2c71eda00ff8f46ffb573eb62c0a1f163cd1e4ea
cofitigar <37101050+cofitigar@users.noreply.github.com>
2019-06-30 13:09:02 +0200

    JPA I JWT

---
+++ InfBezWeb/infbez/src/main/java/com/infbez/repository/AuthorityRepository.java
@@ -0,0 +1,14 @@
+package com.infbez.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.infbez.model.Authority;
+
+
+
+public interface AuthorityRepository extends JpaRepository<Authority, Long> {
+	
+	Authority findByName(String name);
+	
+
+}
---
+++ InfBezWeb/infbez/src/main/java/com/infbez/rest/UserController.java
@@ -0,0 +1,114 @@
+package com.infbez.rest;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import com.infbez.DTO.UserDTO;
+import com.infbez.model.Authority;
+import com.infbez.model.User;
+import com.infbez.service.AuthorityServiceInterface;
+import com.infbez.service.UserServiceInterface;
+
+import java.security.cert.Certificate;
+
+
+
+@RestController
+@RequestMapping(value = "api/users")
+@CrossOrigin("*")
+public class UserController {
+	
+
+	private UserServiceInterface userService;
+	private AuthorityServiceInterface authorityService;
+	private PasswordEncoder passwordEncoder;
+
+	public UserController(UserServiceInterface userService, AuthorityServiceInterface authorityService, PasswordEncoder passwordEncoder){
+		this.userService = userService;
+		this.authorityService = authorityService;
+		this.passwordEncoder = passwordEncoder;
+	}
+
+
+
+	@GetMapping
+	public List<User> getAll() {
+        return this.userService.findAll();
+    }
+
+
+	@GetMapping(value = "/inactive")
+	public ResponseEntity<List<UserDTO>>getInactive(){
+		List<UserDTO> inactive = new ArrayList<>();
+		List<User> users = userService.findByActiveFalse();
+		for (User user : users) {
+			inactive.add(new UserDTO(user));
+		}
+		return new ResponseEntity<>(inactive,HttpStatus.OK);
+	}
+
+
+	@GetMapping(value = "/active")
+	public ResponseEntity<List<UserDTO>> getActive(){
+		List<UserDTO> active = new ArrayList<>();
+		List<User> users = userService.findByActiveTrue();
+		for (User user : users) {
+			active.add(new UserDTO(user));
+		}
+		return new ResponseEntity<>(active,HttpStatus.OK);
+	}
+	
+	
+	
+	@PostMapping(value="/register", consumes="application/json")
+	public ResponseEntity<UserDTO> register(@RequestBody UserDTO userDTO) {
+		Authority authority = authorityService.findByName("REGULAR");
+		
+		User u = userService.findByEmail(userDTO.getEmail());
+		if(u!=null) {
+			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+		}
+		
+		
+		u = new User();
+		u.setEmail(userDTO.getEmail());
+		u.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+		u.setActive(false);
+		u.getUser_authorities().add(authority);
+		
+
+		u = userService.save(u);
+		return new ResponseEntity<>(new UserDTO(u),HttpStatus.OK);
+	}
+
+	@PutMapping(value="/activate/{id}")
+	public ResponseEntity<UserDTO> activateUser(@PathVariable("id") Long id){
+		User user = userService.findById(id);
+		if(user == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		user.setActive(true);
+		user = userService.save(user);
+		return new ResponseEntity<>(new UserDTO(user),HttpStatus.OK);
+	}
+
+
+	@RequestMapping("/whoami")
+	public User user(Principal user) {
+		return this.userService.findByEmail(user.getName());
+	}
+
+}
+
